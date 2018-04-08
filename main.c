77c5317ebd8b0fb67fe5bc23fd98084a97ee5cc7
Wellington Correia <wellington.correia02@gmail.com>
2018-04-08 15:48:03 -0300

    Fazendo a leitura do genoma.

--- main.c
+++ main.c
@@ -3,6 +3,24 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int initGenome = 22983, finishGenome = 25781, countInitGenome = 0;
+
+    char url[]="Complete_Genome.txt";
+	char ch;
+	FILE *arq;
+
+	arq = fopen(url, "r");
+	if(arq == NULL){
+	    printf("Erro, nao foi possivel abrir o arquivo\n");
+	}else{
+	    while(ch != EOF && countInitGenome != finishGenome){
+            countInitGenome++;
+            ch = fgetc(arq);
+            if(countInitGenome >= initGenome){
+                putchar(ch);
+            }
+	    }
+	}
+	fclose(arq);
     return 0;
 }

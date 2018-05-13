2b317f3b5ca9c81ad5207e0a990eb517d30765ec
Samuel <sam.scarvalho17@gmail.com>
2018-05-13 20:55:18 -0300

    teste

--- main.c
+++ main.c
@@ -14,10 +14,19 @@ int main(){
     int optimalDistance = 30;
     int countPreencherVetor = 0;
     int tamanhoVetor = 0;
-    char restricTest[10];
+    int contRestric = 0;
+    int quantRestrics = 0;
+    int RestricInit = 0;
+    int RestricFinish = 0;
+    int tamStringRest = 0;
+    int posicaoGenoma = 0;
+
     //int x =0;
 
+
     char url[]="Complete_Genome.txt";
+    char restric[] = "GAATTC";
+    char restricTest[8];
     char ch;
     FILE *arq;
 
@@ -30,6 +39,10 @@ int main(){
     scanf("%d",&ordem);
     printf("Total de caracters para analise do promotor \n");
     scanf("%d",&optimalDistance);
+    printf("Digite a restricao \n");
+    scanf("%s",&restric);
+
+    tamStringRest = strlen(restric);
 
 
     tamanhoVetor = ((finishGenome) - initGenome)+1;
@@ -63,6 +76,47 @@ int main(){
     else
         buscarImprimirPromotorPositivo(vetorPromoter, initGenome, optimalDistance);
 
-    restricts(countPreencherVetor, restricTest, initGenome, finishGenome);
+    //restricts(restric, initGenome, finishGenome);
+
+    fclose(arq);
+    arq = fopen(url,"r");
+    contRestric = 0;
+    countPreencherVetor = 0;
+    if(arq == NULL){
+       printf("Erro, nao foi possivel abrir o arquivo\n");
+    }else{
+       while(ch != EOF){
+            ch = fgetc(arq);
+            if(ch=='A' || ch== 'T' || ch== 'G' || ch=='C'){
+                restricTest[contRestric] = ch;
+                contRestric++;
+                RestricFinish++;
+                countPreencherVetor++;
+            }
+            if(contRestric==tamStringRest){
+                contRestric = 0;
+                if(strcmp(restric,restricTest)==0){
+                    quantRestrics ++;
+                    printf("\n Restrict n:%d. Posicao:%d|%d \n",quantRestrics,RestricInit,RestricFinish);
+                    tamanhoVetor = RestricFinish;
+                    if(RestricInit <= initGenome && RestricFinish>=finishGenome){
+                        printf("Seu genoma esta presente no Restrict n:%d\n",quantRestrics);
+                        posicaoGenoma = quantRestrics;
+                    }
+                    RestricInit = RestricFinish+1;
+                    ch = fgetc(arq);
+                    countPreencherVetor++;
+                }else{
+                    countPreencherVetor-=(tamStringRest-1);
+                    RestricFinish-=(tamStringRest-1);
+                    fseek(arq,countPreencherVetor,SEEK_SET);
+                }
+            }
+
+        }
+        printf("\n    Numero de bases na sequencia: %d\n",tamanhoVetor);
+        printf("    Numero de fragmentos de restricao: %d\n",quantRestrics);
+        printf("    O fragmento %d contem o gene em estudo\n",posicaoGenoma);
+    }
     return 0;
 }
